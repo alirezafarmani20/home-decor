d93ff7423d9f0d9a65ea2d8b766211f306a54ff2
alireza <farmanialireza668@gmail.com>
2025-03-23 12:50:14 +0330

    set color in project

---
+++ lib/core/theme.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/services.dart';
+
+class AppTheme {
+  static const Color white = Color(0xffFFFFFF);
+  static const Color black = Color(0xff363130);
+  static const Color salmon = Color(0xffF4B5A4);
+  static const Color terracotta = Color(0xffCC7861);
+  static const Color brownRosy = Color(0xffDCBEB6);
+  static const Color beige = Color(0xffFAF0E6);
+  static const Color orangeDark = Color(0xff4B4544);
+}
---
+++ lib/screens/lunch/lunch_screen.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class LunchScreen extends StatefulWidget {
+  const LunchScreen({super.key});
+
+  @override
+  State<LunchScreen> createState() => _LunchScreenState();
+}
+
+class _LunchScreenState extends State<LunchScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("hello world")
+          ],
+        ),
+      ),
+    );
+  }
+}
