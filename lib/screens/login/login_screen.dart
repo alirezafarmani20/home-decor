bbf39b4a11d5ea728dddc6b16c43bdae8083f417
alireza <farmanialireza668@gmail.com>
2025-03-29 12:28:55 +0330

    debug appbar and some changes

--- lib/screens/login/login_screen.dart
+++ lib/screens/login/login_screen.dart
@@ -1,4 +1,8 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+//import 'package:home_decor_app/main.dart';
+import 'package:home_decor_app/screens/onboarding/onboarding_screen4.dart';
+import 'package:home_decor_app/widgets/appbar.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -8,8 +12,29 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  static const pages = OnboardingScreen4();
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final size = MediaQuery.of(context).size.width;
+    return Scaffold(
+      body: SafeArea(child: Padding(
+        padding: EdgeInsets.all(14),
+        child: Column(
+          children: [
+            Column(
+              //mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                // ignore: sized_box_for_whitespace
+                Container(
+                  width:size ,
+                  height: 50,
+                  child: MyAppbar(title: "Log In",page: pages,),
+                )
+              ],
+            )
+          ],
+        ),
+      )),
+    );
   }
 }
--- lib/widgets/appbar.dart
+++ lib/widgets/appbar.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:home_decor_app/core/theme.dart';
+//import 'package:home_decor_app/core/theme.dart';
+//import 'package:home_decor_app/screens/onboarding/onboarding_screen4.dart';
 
 class MyAppbar extends StatelessWidget {
   final String title;
@@ -9,31 +11,33 @@ class MyAppbar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Row(
-            children: [
-              TextButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => page),
-                  );
-                },
-                child: Text("hello world"),
-              ),
-              Text(
+      body: Align(
+        alignment: Alignment.center,
+        child: Row(
+          children: [
+            TextButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => page),
+                );
+              },
+              child: Image(image: AssetImage("lib/assets/icons/leftarow.png")),
+            ),
+            Padding(
+              padding: EdgeInsets.only(left: 90),
+              child: Text(
                 title,
                 style: TextStyle(
                   color: AppTheme.salmon,
-                  fontSize: 20,
+                  fontSize: 25,
                   fontWeight: FontWeight.bold,
                 ),
                 textAlign: TextAlign.center,
               ),
-            ],
-          ),
-        ],
+            ),
+          ],
+        ),
       ),
     );
   }
